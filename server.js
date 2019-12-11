2f001d3286f9eadd9a8f7421a49dd8939ee3f1c6
vennhost <samogconsult@gmail.com>
2019-12-11 23:38:21 +0100

    student woking fine

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const router = require("./src/services/students/")
 
 const port = 3001;
 
+server.use(express.json())
+
 server.use("/students", router)
 
-server.use(express.json())
+
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
